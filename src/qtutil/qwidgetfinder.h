6dab1d60d1258d46c67ffd51cbab658c94629d12
TrickyLeifa <date.epoch@gmail.com>
2024-05-31 14:39:11 +0200

    Added fatal error to qtutil::QWidgetFinder
    * Added fatal error to qtutils::QWidgetFinder
      * Previously, the user would have to check themselves if the pointer provided is null. Now it simply report a fatal error when it fails to.

--- src/qtutil/qwidgetfinder.h
+++ src/qtutil/qwidgetfinder.h
@@ -13,6 +13,10 @@ public:
   void findChild(T *&child, const QString &name, Qt::FindChildOptions options = Qt::FindChildrenRecursively)
   {
     child = m_root->findChild<T *>(name, options);
+    if (!child)
+    {
+      qFatal("Could not find child widget with name %s", qPrintable(name));
+    }
   }
 
 private:
